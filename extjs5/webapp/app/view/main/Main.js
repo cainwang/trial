e6129caa42d1c134ae868d11a6cfc213f38c359e
Cain Wang <cainwang@vmware.com>
2014-07-02 17:11:57 -0700

    After sencha build.

--- extjs5/webapp/app/view/main/Main.js
+++ extjs5/webapp/app/view/main/Main.js
@@ -1,7 +1,7 @@
 /**
  * This class is the main view for the application. It is specified in app.js as the
- * "autoCreateViewport" property. That setting automatically applies the "viewport"
- * plugin to promote that instance of this class to the body element.
+ * "autoCreateViewport" property. That setting automatically applies the "viewport" plugin to
+ * promote that instance of this class to the body element.
  *
  * TODO - Replace this content of this view to suite the needs of your application.
  */
@@ -9,7 +9,7 @@ Ext.define('extjs5.view.main.Main', {
     extend: 'Ext.container.Container',
 
     xtype: 'app-main',
-    
+
     controller: 'main',
     viewModel: {
         type: 'main'
@@ -19,7 +19,13 @@ Ext.define('extjs5.view.main.Main', {
         type: 'border'
     },
 
-    items: [{
+    items: [ {
+        region: 'north',
+        xtype: 'component',
+        cls: 'app-banner',
+        html: 'My Extjs 5 App',
+        padding: 10
+    }, {
         xtype: 'panel',
         bind: {
             title: '{name}'
@@ -28,16 +34,20 @@ Ext.define('extjs5.view.main.Main', {
         html: '<ul><li>This area is commonly used for navigation, for example, using a "tree" component.</li></ul>',
         width: 250,
         split: true,
-        tbar: [{
+        collapsible: true,
+        tbar: [ {
             text: 'Button',
             handler: 'onClickButton'
-        }]
-    },{
+        } ]
+    }, {
         region: 'center',
         xtype: 'tabpanel',
-        items:[{
+        items: [ {
             title: 'Tab 1',
             html: '<h2>Content appropriate for the current navigation.</h2>'
-        }]
-    }]
+        }, {
+            title: 'Light',
+            html: 'I stand for the light.'
+        } ]
+    } ]
 });
