ed472e5281065dc582ea1bb87b944e2fd27bdf34
Cain Wang <starwhisperer@gmail.com>
2014-10-12 22:34:09 -0700

    Angular grunt structure

--- angular/Gruntfile.js
+++ angular/Gruntfile.js
@@ -1,12 +1,75 @@
 module.exports = function(grunt) {
-    grunt.loadNpmTasks('grunt-wiredep');
+    require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
-        webapp: 'webapp',
+        appDir: 'app',
+        buildDir: 'dist',
         wiredep: {
             target: {
-                src: '<%= webapp%>/index.html'
+                src: '<%=appDir%>/index.html'
+            }
+        },
+        jshint: {
+            options: {
+                reporter: require('jshint-stylish')
+            },
+            all: ["<%=appDir%>/scripts/**/*.js"]
+        },
+        less: {
+            options: {
+                paths: ['<%=appDir%>/resources/less']
+            },
+            dev: {
+                files: {
+                    '<%=appDir%>/resources/styles.css': '**/*/styles.less'
+                }
+            },
+            prod: {
+                options: {
+                    compress: true
+                },
+                files: {
+                    '<%=buildDir%>/resources/styles.css': '**/*/styles.less'
+                }
+            }
+        },
+        copy: {
+            prod: {
+                files: {
+                    '<%=buildDir%>/index.html': '<%=appDir%>/index.prod.html'
+                }
+            }
+        },
+        concat: {
+            prod: {
+                files: {
+                    '<%=buildDir%>/scripts/app-all.js': '<%=appDir%>/scripts/**/*.js'
+                }
+            }
+        },
+        ngAnnotate: {
+            options: {
+                singleQuotes: true
+            },
+            prod: {
+                files: {
+                    '<%=buildDir%>/scripts/app-all.js': '<%=buildDir%>/scripts/app-all.js'
+                }
+            }
+        },
+        uglify: {
+            prod: {
+                files: {
+                    '<%=buildDir%>/scripts/app-all.min.js': '<%=buildDir%>/scripts/app-all.js'
+                }
+            }
+        },
+        clean: {
+            target: {
+                src: '<%=buildDir%>'
             }
         }
     });
+
+    grunt.registerTask('default', ['clean', 'jshint', 'less:prod', 'copy', 'concat', 'ngAnnotate', 'uglify']);
 };
