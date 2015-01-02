229be21c39b4ea9e3f25b297dabf6e9677a3e43a
Cain Wang <starwhisperer@gmail.com>
2015-01-02 15:12:52 -0800

    Update node express example

--- node/src/server-hello.js
+++ node/src/server-hello.js
@@ -2,8 +2,15 @@ var router = require('express').Router();
 
 router.get('/:name', function(req, res) {
     res.status(200).json({
-        message: 'Hello, ' + req.params.name + '!'
+        message: 'Hello, ' + req.params.name
     });
 });
+
+router.post('/', function(req, res) {
+    var body = req.body;
+    res.status(200).json({
+        message: 'Hello, ' + body.name
+    });
+})
 
 module.exports = router;
--- node/src/server.js
+++ node/src/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 var path = require('path');
 
 app.use(function(req, res, next) {
@@ -15,5 +18,5 @@ app.use('/hello', require('./server-hello'));
 
 app.use(express.static(path.join(__dirname, '..')));
 
-console.log('Experss server started ...');
-app.listen(8088);
+console.log('Experss server started at localhost:8080');
+app.listen(8080);
